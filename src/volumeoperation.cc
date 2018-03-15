f3777d4cf6cc83876ebd52218a0c194c74ad6264
Marijn Stollenga <m.stollenga@gmail.com>
2018-03-15 14:24:39 +0100

    remove global validation thingy

--- src/volumeoperation.cc
+++ src/volumeoperation.cc
@@ -1,6 +1,6 @@
 #include "volumeoperation.h"
 #include "vlstm.h"
-#include "global.h"
+//#include "global.h"
 
 using namespace std;
 
@@ -19,12 +19,12 @@ void FCVolumeOperation::forward(Volume &in, Volume &out)  {
 	tin.data = in.data();
 	tout.data = out.data();
 	op.forward(tin, tout);
-	if(dropout > 0.0) {
-	  if (Global::validation())
-	    (*out.buf) *= 1.0 - dropout;
-	  else
+	//if(dropout > 0.0) {
+    //if (Global::validation())
+    //  (*out.buf) *= 1.0 - dropout;
+    //else
 	    out.dropout(dropout);
-	}
+        //}
 }
 
 void FCVolumeOperation::backward_weights(VolumeSet &in, VolumeSet &out){
